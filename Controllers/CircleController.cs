662d27c75f3fdf26c4c25e4d77e09203b79c1e74
pkd11011 <130068719+pkd11011@users.noreply.github.com>
2025-02-05 14:02:32 +0700

    Add files via upload

---
+++ Controllers/CircleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CircleController : ControllerBase
+    {
+        [HttpGet("cv_dt")]
+        public IActionResult tinh_chuvi_dientich_duongkinh(double rr) // rr là bán kính
+        {
+            if (rr <= 0)
+            {
+                return BadRequest("Bán kính phải dương");
+            }
+
+            double cv = 3.14 * 2 * rr;   // Chu vi
+            double dt = 3.14 * rr * rr; // Diện tích
+            double dk = 2 * rr;         // Đường kính
+
+            var json_str = new { dien_tich = dt, chu_vi = cv, duong_kinh = dk };
+            return Ok(json_str);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = "Swagger"; 
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run("http://localhost:5276");
